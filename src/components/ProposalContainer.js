bb0e9ce82073b783b1ae178178d504d17b349c49
nikhilthakur <nikhil.thakur.java@gmail.com>
2025-03-07 04:55:53 -0800

    Removed left flowing nav

--- src/components/ProposalContainer.js
+++ src/components/ProposalContainer.js
@@ -41,74 +41,7 @@ const ProposalContainer = () => {
 
     return (
         <div className="relative">
-            {/* Fixed Navigation */}
-            <div className="fixed left-0 top-1/2 transform -translate-y-1/2 z-50">
-                <div className="bg-white shadow-lg rounded-r-lg py-4 px-2">
-                    <ul className="space-y-6">
-                        <li>
-                            <button
-                                onClick={() => scrollToSection(overviewRef)}
-                                className={`p-2 rounded-full focus:outline-none transition-all duration-300 ${
-                                    activeSection === 'overview'
-                                        ? 'bg-orange-500 text-white shadow-md'
-                                        : 'text-gray-500 hover:text-orange-500'
-                                }`}
-                                title="Overview"
-                            >
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                                </svg>
-                            </button>
-                        </li>
-                        <li>
-                            <button
-                                onClick={() => scrollToSection(timelineRef)}
-                                className={`p-2 rounded-full focus:outline-none transition-all duration-300 ${
-                                    activeSection === 'timeline'
-                                        ? 'bg-orange-500 text-white shadow-md'
-                                        : 'text-gray-500 hover:text-orange-500'
-                                }`}
-                                title="Timeline"
-                            >
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
-                                </svg>
-                            </button>
-                        </li>
-                        <li>
-                            <button
-                                onClick={() => scrollToSection(servicesRef)}
-                                className={`p-2 rounded-full focus:outline-none transition-all duration-300 ${
-                                    activeSection === 'services'
-                                        ? 'bg-orange-500 text-white shadow-md'
-                                        : 'text-gray-500 hover:text-orange-500'
-                                }`}
-                                title="Services"
-                            >
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd" />
-                                    <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
-                                </svg>
-                            </button>
-                        </li>
-                        <li>
-                            <button
-                                onClick={() => scrollToSection(commercialsRef)}
-                                className={`p-2 rounded-full focus:outline-none transition-all duration-300 ${
-                                    activeSection === 'commercials'
-                                        ? 'bg-orange-500 text-white shadow-md'
-                                        : 'text-gray-500 hover:text-orange-500'
-                                }`}
-                                title="Investment"
-                            >
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fillRule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-                                </svg>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+
 
             {/* Progress indicator at top */}
             <div className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md">
